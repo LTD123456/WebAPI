10a91c735df56d4e11bb538eb440621bab243c5c
LTD123456 <Lethienduc123@gmail.com>
2019-12-31 17:37:45 +0700

    create context

---
+++ WebAPI/DBContext/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.DBContext
+{
+    public class StudentContext:DbContext
+    {
+        public DbSet<Student> Students { set; get; }
+        public DbSet<Course> Courses { set; get; }
+        public StudentContext (DbContextOptions option):base(option)
+        { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        { }
+    }
+}
